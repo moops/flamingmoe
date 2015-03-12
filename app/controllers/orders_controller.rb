dc71bc889c5c7378daf7b59872c528d85d7a2f60
Adam <adam@raceweb.ca>
2015-03-11 23:40:03 -0700

    some style

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,2 +1,32 @@
 class OrdersController < ApplicationController
+
+  # GET /orders
+  # GET /orders.json
+  def index
+    @order = Order.new # for the new order form
+  end
+
+  # POST /orders
+  # POST /orders.json
+  def create
+    @order = Order.new(order_params)
+
+    respond_to do |format|
+      if @order.save
+        format.html { redirect_to @order, notice: 'Order was successfully created.' }
+        format.json { render :show, status: :created, location: @order }
+        format.js
+      else
+        format.html { render :new }
+        format.json { render json: @order.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  private
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def order_params
+      params.require(:order).permit(:quantity, :product_id)
+    end
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -3,4 +3,6 @@ class Order < ActiveRecord::Base
   belongs_to :customer
   belongs_to :product
 
+  accepts_nested_attributes_for :customer
+
 end
