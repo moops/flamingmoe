9463be7c9d6ebf9a685997e841d3ba379714a6f0
Adam <adam@raceweb.ca>
2015-03-12 19:35:50 -0700

    add order controller tests

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -3,30 +3,46 @@ class OrdersController < ApplicationController
   # GET /orders
   # GET /orders.json
   def index
-    @order = Order.new # for the new order form
+
+    # the order needs a blank customer for the new order form
+    @customer = Customer.new
+    @order = @customer.orders.build
   end
 
   # POST /orders
   # POST /orders.json
   def create
     @order = Order.new(order_params)
+    
+    customer_params = order_params[:customer_attributes]
+    logger.debug("building order customer with: #{customer_params.inspect}")
+
+    # find a customer by email or build a new one
+    @customer = Customer.where('email = ?', customer_params[:email]).first if customer_params
+    @customer = Customer.new(customer_params) unless @customer
+    @order.customer = @customer
 
     respond_to do |format|
       if @order.save
         format.html { redirect_to @order, notice: 'Order was successfully created.' }
         format.json { render :show, status: :created, location: @order }
-        format.js
+        format.js { render :create }
       else
         format.html { render :new }
         format.json { render json: @order.errors, status: :unprocessable_entity }
+        format.js
       end
     end
   end
 
   private
 
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def order_params
-      params.require(:order).permit(:quantity, :product_id)
-    end
+  # white list parameters
+  def order_params
+    params.require(:order).permit(
+      :quantity,
+      :product_id,
+      customer_attributes: [:id, :name, :email, :address, :city, :country, :credit_card, :expiry])
+  end
+
 end
--- test/controllers/orders_controller_test.rb
+++ test/controllers/orders_controller_test.rb
@@ -1,7 +1,33 @@
 require 'test_helper'
 
 class OrdersControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  
+  test "should get index" do
+    get :index
+    assert_response :success
+    assert_not_nil assigns(:order)
+    assert_not_nil assigns(:order).customer, '@order should have a blank customer'
+  end
+
+  test "should create order" do
+    assert_difference('Order.count') do
+      xhr :post, :create, order: {
+        quantity: 2,
+        product_id: 1,
+        customer_attributes: {
+          name: 'homer',
+          email: 'homer@example.com',
+          address: '724 evergreen terrace',
+          city: 'sprinfield',
+          country: 'usa',
+          credit_card: '4111111111111111',
+          expiry: '012018'
+        }
+      }
+    end
+
+    assert_response :success
+    assert_template :create
+  end
+
 end
