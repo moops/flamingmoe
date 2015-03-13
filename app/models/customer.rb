a90cf228f70ba8fb8491e97436b5f9681f8b18b6
Adam <adam@raceweb.ca>
2015-03-12 19:33:58 -0700

    add email validation and test

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,17 +1,14 @@
+require 'email_validator'
+
 class Customer < ActiveRecord::Base
 
   has_many :orders
 
-  validates :name, presence: true
-  validates :email, presence: true
+  validates :name, :email, :address, :city, :country, :credit_card, :expiry, presence: true
   validates :email, uniqueness: true
-  validates :address, presence: true
-  validates :city, presence: true
-  validates :country, presence: true
-  validates :credit_card, presence: true
-  validates :expiry, presence: true
+  validates :email, email: true
   validates :expiry, format: {
-    with: /[0-1]\d{5}/,
+    with: /\A[0-1]\d{5}\z/,
     message: "format must be mmyyyy"
   }
 
--- test/models/customer_test.rb
+++ test/models/customer_test.rb
@@ -4,37 +4,55 @@ class CustomerTest < ActiveSupport::TestCase
   
   test 'name is required' do
     customer = build(:customer, name: nil)
-    assert_not customer.save, 'customer should require a name'
+    assert_not customer.valid?, 'customer should require a name'
   end
 
   test 'email is required' do
     customer = build(:customer, email: nil)
-    assert_not customer.save, 'customer should require an email'
+    assert_not customer.valid?, 'customer should require an email'
+  end
+
+  test 'email format' do
+    customer = build(:customer, email: 'not_an_email')
+    assert_not customer.valid?, 'customer should require a proper email'
   end
 
   test 'address is required' do
     customer = build(:customer, address: nil)
-    assert_not customer.save, 'customer should require an address'
+    assert_not customer.valid?, 'customer should require an address'
   end
 
   test 'city is required' do
     customer = build(:customer, city: nil)
-    assert_not customer.save, 'customer should require a city'
+    assert_not customer.valid?, 'customer should require a city'
   end
 
   test 'country is required' do
     customer = build(:customer, country: nil)
-    assert_not customer.save, 'customer should require a country'
+    assert_not customer.valid?, 'customer should require a country'
   end
 
   test 'credit card is required' do
     customer = build(:customer, credit_card: nil)
-    assert_not customer.save, 'customer should require a credit card'
+    assert_not customer.valid?, 'customer should require a credit card'
   end
 
   test 'expiry is required' do
     customer = build(:customer, expiry: nil)
-    assert_not customer.save, 'customer should require an expiry'
+    assert_not customer.valid?, 'customer should require an expiry'
+  end
+
+  test 'expiry format' do
+    customer = build(:customer, expiry: '01201y')
+    assert_not customer.valid?, 'expiry format should only contain numbers'
+    customer.expiry = '01201'
+    assert_not customer.valid?, 'expiry format should not be less than six characters'
+    customer.expiry = '0120188'
+    assert_not customer.valid?, 'expiry format should not be more than six characters'
+    customer.expiry = '212018'
+    assert_not customer.valid?, 'expiry format should start with a 0 or 1'
+    customer.expiry = '012018'
+    assert customer.valid?, 'expiry format should be valid'
   end
 
 end
